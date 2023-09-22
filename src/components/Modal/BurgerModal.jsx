c7093a360dadf3159868fd8dcaea940508ceeedf
Bobshop verbytski <nikolasdelunko@gmail.com>
2023-09-22 12:55:17 +0200

    start migrate from redux to nano in Modal component

--- src/components/Modal/BurgerModal.jsx
+++ src/components/Modal/BurgerModal.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import Menu from "./Menu/Menu";
+import { page } from "../../store/helpersStore";
 
 export default function BurgerModal() {
-  const page = useSelector((state) => state.helpers.page);
+  // const page = useSelector((state) => state.helpers.page);
   return (
     <>
       {page == "main" ? (
-        <div className="w-[100%] h-[500px] absolute bg-main-color mt-[85px] flex justify-center flex-col z-20">
+        <div className="absolute z-20 mt-[85px] flex h-[500px] w-[100%] flex-col justify-center bg-main-color">
           <Menu />
         </div>
       ) : (
-        <div className="w-[100%] h-[500px] absolute bg-main-color flex justify-center flex-col z-20">
+        <div className="absolute z-20 flex h-[500px] w-[100%] flex-col justify-center bg-main-color">
           <Menu />
         </div>
       )}
--- src/components/Modal/ModalMobSticker.jsx
+++ src/components/Modal/ModalMobSticker.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // import { useDispatch } from "react-redux";
 // import { setModalStickerMobile } from "../../store/helpers/helpersSlice";
 import Cross from "../Theme/icons/Cross";
+import {setModalStickerMobile} from "../../store/helpersStore"
 
 export default function ModalMobSticker({ content }) {
   // const dispatch = useDispatch();
@@ -12,6 +13,7 @@ export default function ModalMobSticker({ content }) {
       className="bg-modal top-0 left-0 w-[100%] h-[80%] block fixed z-50 overflow-y-auto"
       onClick={(e) => {
         // dispatch(setModalStickerMobile(false));
+				setModalStickerMobile(false)
       }}
     >
       <div
@@ -23,6 +25,7 @@ export default function ModalMobSticker({ content }) {
             className="absolute w-[24px] h-[24px] bg-[#F9E3C6] rounded-[50%] left-[1em] top-[1.5em] cursor-pointer"
             onClick={() => {
               // dispatch(setModalStickerMobile(false));
+							setModalStickerMobile(false)
             }}
           >
             <Cross />
