a1f493f28cf52bca841799f16475909841abb902
Bobshop verbytski <nikolasdelunko@gmail.com>
2023-10-03 12:37:10 +0200

    rewrite stickers store

--- src/store/stickers.ts
+++ src/store/stickers.ts
@@ -1,34 +1,59 @@
 import { atom } from "nanostores";
 const userPay = localStorage.getItem("userPayStickers");
 
-const initialState = {
+// const initialState = {
+//   listPhoto: null,
+//   pay: userPay === "true" ? true : false || false,
+//   client: JSON.parse(localStorage.getItem("client")) || [],
+// };
+interface StickersStoreState {
+  listPhoto: null;
+  pay: boolean;
+	client: null;
+}
+
+// const stickersStore = createStore(initialState);
+
+const stickersStore = atom<StickersStoreState>({
   listPhoto: null,
   pay: userPay === "true" ? true : false || false,
   client: JSON.parse(localStorage.getItem("client")) || [],
-};
+});
 
-const stickersStore = createStore(initialState);
+// export const useStickers = stickersStore.subscribe;
 
-export const useStickers = stickersStore.subscribe;
-
-export function setListPhoto(payload) {
-  stickersStore.update((state) => {
-    state.listPhoto = payload;
-  });
+export function setListPhoto(Photo: <T>) {
+  stickersStore.set({ ...stickersStore.get(), listPhoto: Photo });
 }
 
-export function setPaySticker(payload) {
-  stickersStore.update((state) => {
-    state.pay = payload;
-    localStorage.setItem("userPayStickers", payload);
-  });
+// export function setListPhoto(payload) {
+//   stickersStore.update((state) => {
+//     state.listPhoto = payload;
+//   });
+// }
+
+export function setPaySticker(Pay: boolean) {
+  stickersStore.set({ ...stickersStore.get(), pay: Pay});
+	localStorage.setItem("userPayStickers", Pay)
 }
 
-export function setClient(payload) {
-  stickersStore.update((state) => {
-    state.client = [...state.client, payload];
-    localStorage.setItem("client", JSON.stringify(state.client));
-  });
+// export function setPaySticker(payload) {
+//   stickersStore.update((state) => {
+//     state.pay = payload;
+//     localStorage.setItem("userPayStickers", payload);
+//   });
+// }
+
+// export function setClient(payload) {
+//   stickersStore.update((state) => {
+//     state.client = [...state.client, payload];
+//     localStorage.setItem("client", JSON.stringify(state.client));
+//   });
+// }
+
+export function setClient(Client: <T>) {
+  stickersStore.set({ ...stickersStore.get(), client: Client});
+	localStorage.setItem("client", JSON.stringify(Client));
 }
 
 export default stickersStore;
