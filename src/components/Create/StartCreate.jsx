92523b68e8fb2ca28b9344c2af267beb665324a2
Bobshop verbytski <nikolasdelunko@gmail.com>
2023-11-10 12:01:01 +0100

    set Listenners

--- src/components/Create/StartCreate.jsx
+++ src/components/Create/StartCreate.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect } from "react";
+import { useStore } from '@nanostores/react'
 import { setPage, step } from "../../store/helpersStore";
 import Step1 from "./Steps/Step1";
 import Step2 from "./Steps/Step2";
 import Step3 from "./Steps/Step3";
 
 export default function StartCreate() {
+
+	const sTep = useStore(step)
   useEffect(() => {
     setPage("create");
-  }, []);
+  }, [sTep]);
 
-	//! need setup listeners
-	step.listen((profile, changed) => {
-		console.log(`${changed} new value ${profile[changed]}`)
-	})
+  //! need setup listeners
+  step.listen((step, changed) => {
+    console.log(
+      `${changed} new value ${[changed]}`,
+      "STEP:",
+      sTep
+    );
+  });
 
   console.log("StartCreate", step.get());
   return (
